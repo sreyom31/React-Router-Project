64ea19b69beab6160220f65f8b91783c8994a9bf
sreyom31 <sreyom31@gmail.com>
2021-10-22 21:10:13 +0530

    redirecting and extracting parameters

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { Redirect, Route, Switch } from "react-router"
+import AllQuotes from "./pages/AllQuotes"
+import QuoteDetail from "./pages/QuoteDetail"
+import NewQuote from "./pages/NewQuote"
+
 function App() {
   return (
-    <div>
-
-    </div>
+    <Switch>
+      <Route path='/' exact>
+        <Redirect to='/quotes' />
+      </Route>
+      <Route path='/quotes' exact>
+        <AllQuotes />
+      </Route>
+      <Route path='/quotes/:quoteId'>
+        <QuoteDetail />
+      </Route>
+      <Route path='/new-quote'>
+        <NewQuote />
+      </Route>
+    </Switch>
   );
 }
-
 export default App;
